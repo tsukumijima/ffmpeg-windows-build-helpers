5623e685cd2d9002e257b2d34c370ba3de2abcaa
tsukumijima <tsukumijima@users.noreply.github.com>
2021-07-01 14:01:12 +0900

    Tweak config options

--- cross_compile_ffmpeg.sh
+++ cross_compile_ffmpeg.sh
@@ -1345,7 +1345,7 @@ build_lame() {
 }
 
 build_twolame() {
-  do_git_checkout https://github.com/njh/twolame.git
+  do_git_checkout https://github.com/njh/twolame.git twolame_git "origin/main"
   cd twolame_git
     if [[ ! -f Makefile.am.bak ]]; then # Library only, front end refuses to build for some reason with git master
       sed -i.bak "/^SUBDIRS/s/ frontend.*//" Makefile.am || exit 1
@@ -2351,7 +2351,7 @@ build_ffmpeg() {
       init_options+=" --disable-schannel"
       # Fix WinXP incompatibility by disabling Microsoft's Secure Channel, because Windows XP doesn't support TLS 1.1 and 1.2, but with GnuTLS or OpenSSL it does.  XP compat!
     fi
-    config_options="$init_options --enable-libcaca --enable-gray --enable-libtesseract --enable-fontconfig --enable-gmp --enable-gnutls --enable-libass --enable-libbluray --enable-libbs2b --enable-libflite --enable-libfreetype --enable-libfribidi --enable-libgme --enable-libgsm --enable-libilbc --enable-libmodplug --enable-libmp3lame --enable-libopencore-amrnb --enable-libopencore-amrwb --enable-libopus --enable-libsnappy --enable-libsoxr --enable-libspeex --enable-libtheora --enable-libtwolame --enable-libvo-amrwbenc --enable-libvorbis --enable-libwebp --enable-libzimg --enable-libzvbi --enable-libmysofa --enable-libopenjpeg  --enable-libopenh264 --enable-liblensfun  --enable-libvmaf --enable-libsrt --enable-demuxer=dash --enable-libxml2 --enable-opengl --enable-libdav1d --enable-cuda-llvm"
+    config_options="$init_options --enable-libcaca --enable-gray --enable-libtesseract --enable-fontconfig --enable-gmp --enable-gnutls --enable-libass --enable-libbluray --enable-libbs2b --enable-libflite --enable-libfreetype --enable-libfribidi --enable-libgme --enable-libgsm --enable-libilbc --enable-libmodplug --enable-libmp3lame --enable-libopencore-amrnb --enable-libopencore-amrwb --enable-libopus --enable-libsnappy --enable-libsoxr --enable-libspeex --enable-libtheora --enable-libtwolame --enable-libvo-amrwbenc --enable-libvorbis --enable-libwebp --enable-libzimg --enable-libzvbi --enable-libmysofa --enable-libopenjpeg  --enable-libopenh264 --enable-liblensfun  --enable-libvmaf --enable-libsrt --enable-demuxer=dash --enable-libxml2 --enable-opengl"
 
     if [ "$bits_target" != "32" ]; then
       config_options+=" --enable-libsvthevc"
@@ -2359,9 +2359,9 @@ build_ffmpeg() {
       # config_options+=" --enable-libsvtvp9"
     fi
 
-    # cuda-llvm, libdav1d, libaribb24 is only supported by ffmpeg 4.2 series or later
-    if [[ $ffmpeg_git_checkout_version != *"n4.0"* ]] && [[ $ffmpeg_git_checkout_version != *"n4.1"* ]] then
-      config_options+=" --enable-cuda-llvm --enable-libdav1d --enable-libaribb24"
+    # cuda-llvm, libdav1d is only supported by ffmpeg 4.2 series or later
+    if [[ $ffmpeg_git_checkout_version != *"n4.0"* ]] && [[ $ffmpeg_git_checkout_version != *"n4.1"* ]]; then
+      config_options+=" --enable-cuda-llvm --enable-libdav1d"
     fi
 
     #aom must be disabled to use SVT-AV1
@@ -2393,13 +2393,17 @@ build_ffmpeg() {
       config_options+=" --disable-libmfx"
     fi
     if [[ $enable_gpl == 'y' ]]; then
-      config_options+=" --enable-gpl --enable-frei0r --enable-filter=frei0r --enable-librubberband --enable-libvidstab --enable-libx264 --enable-libx265 --enable-avisynth --enable-libaribb24"
+      config_options+=" --enable-gpl --enable-frei0r --enable-filter=frei0r --enable-librubberband --enable-libvidstab --enable-libx264 --enable-libx265 --enable-avisynth"
       config_options+=" --enable-libxvid --enable-libdavs2"
       if [[ $host_target != 'i686-w64-mingw32' ]]; then
         config_options+=" --enable-libxavs2"
       fi
       if [[ $compiler_flavors != "native" ]]; then
         config_options+=" --enable-libxavs" # don't compile OS X
+      fi
+      # libaribb24 is only supported by ffmpeg 4.2 series or later
+      if [[ $ffmpeg_git_checkout_version != *"n4.0"* ]] && [[ $ffmpeg_git_checkout_version != *"n4.1"* ]]; then
+        config_options+=" --enable-libaribb24"
       fi
     fi
     local licensed_gpl=n # lgpl build with libx264 included for those with "commercial" license :)
